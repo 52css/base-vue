0f353d61c8108c36862ea1ea251c5289de4a9834
wèijié <weijie@rongyi.com>
2024-06-29 15:27:01 +0800

    wip: 增加2个todo

---
+++ src/components/base-screenshot.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export interface BaseScreenshotProps {
+  prop1?: string
+}
+export const BaseScreenshotDefault = {}
+export interface BaseScreenshotEmits {
+  (event: 'event1'): void
+}
+</script>
+<script setup lang="ts">
+withDefaults(defineProps<BaseScreenshotProps>(), BaseScreenshotDefault)
+defineEmits<BaseScreenshotEmits>()
+defineOptions({
+  name: 'BaseScreenshot',
+})
+</script>
+
+<template>
+  <div class="base-screenshot">
+    base-screenshot, TODO: 对某个区域转换成svg，再保存图片
+  </div>
+</template>
+
+<style scoped lang="scss">
+.base-screenshot {
+}
+</style>
---
+++ src/components/base-simple-editor.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export interface BaseSimpleEditorProps {
+  prop1?: string
+}
+export const BaseSimpleEditorDefault = {}
+export interface BaseSimpleEditorEmits {
+  (event: 'event1'): void
+}
+</script>
+<script setup lang="ts">
+withDefaults(defineProps<BaseSimpleEditorProps>(), BaseSimpleEditorDefault)
+defineEmits<BaseSimpleEditorEmits>()
+defineOptions({
+  name: 'BaseSimpleEditor',
+})
+</script>
+
+<template>
+  <div class="base-simple-editor">
+    base-simple-editor, TODO: 富文本编辑器, 实现可以添加标签
+  </div>
+</template>
+
+<style scoped lang="scss">
+.base-simple-editor {
+}
+</style>
