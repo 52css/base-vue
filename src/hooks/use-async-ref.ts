0eef82c4b423a458452ccf1f83b441867dc0fce0
52css <jie_wei@126.com>
2024-07-25 15:19:19 +0800

    去掉noMore逻辑

--- src/hooks/use-async-ref.ts
+++ src/hooks/use-async-ref.ts
@@ -4,11 +4,6 @@ export type UseAsyncRefFn = (...args: any) => Promise<any>;
 export type UseAsyncRefOptions<T> = {
   autoFetch?: boolean;
   defaultValue?: T;
-  setResponse?: <R>(
-    response: R,
-    request: Record<string, any>,
-    noMore: Ref<boolean>
-  ) => R;
 };
 export type UseAsyncRefRun = (...args: any[]) => void;
 export type UseAsyncRefResponse<T> = [
@@ -16,13 +11,11 @@ export type UseAsyncRefResponse<T> = [
   UseAsyncRefRun,
   Ref<boolean>,
   Ref<Error>,
-  Ref<boolean>
 ] & {
   data: Ref<UnwrapRef<T> | undefined>;
   run: UseAsyncRefRun;
   loading: Ref<boolean>;
   err: Ref<Error>;
-  noMore: Ref<boolean>;
 };
 
 export const useAsyncRef = <T>(
@@ -31,22 +24,19 @@ export const useAsyncRef = <T>(
 ): UseAsyncRefResponse<T> => {
   const autoFetch = options?.autoFetch ?? true;
   const defaultValue = options?.defaultValue;
-  const setResponseDefault = <R>(x: R) => x;
-  const setResponse = options?.setResponse ?? setResponseDefault;
   const data = ref<T | undefined>(defaultValue);
   const loading = ref(false);
   const err = ref();
-  const noMore = ref(false);
   // 增加状态
 
   const run = (...args: any[]) => {
-    if (loading.value || err.value || noMore.value) {
+    if (loading.value || err.value) {
       return;
     }
     loading.value = true;
     fn(...args)
       .then((res) => {
-        data.value = setResponse(res, args, noMore);
+        data.value = res;
       })
       .catch((e) => {
         err.value = e;
@@ -60,13 +50,12 @@ export const useAsyncRef = <T>(
     run();
   }
 
-  const rtv = [data, run, loading, err, noMore] as UseAsyncRefResponse<T>;
+  const rtv = [data, run, loading, err] as UseAsyncRefResponse<T>;
 
   rtv.data = data;
   rtv.run = run;
   rtv.loading = loading;
   rtv.err = err;
-  rtv.noMore = noMore;
 
   return rtv;
 };
